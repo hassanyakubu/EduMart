e7c0bff9cdceb694d48a5ba19cf5f4eff1bd0db8
Hassan Yakubu <yhassan677@gmail.com>
2025-11-27 16:57:23 +0000

    error debugging

--- app/views/auth/register_process.php
+++ app/views/auth/register_process.php
@@ -1,6 +1,13 @@
 <?php
-require_once __DIR__ . '/../../controllers/auth_controller.php';
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
-$controller = new auth_controller();
-$controller->register();
+try {
+    require_once __DIR__ . '/../../controllers/auth_controller.php';
+
+    $controller = new auth_controller();
+    $controller->register();
+} catch (Exception $e) {
+    die("Registration error: " . $e->getMessage() . "<br>File: " . $e->getFile() . "<br>Line: " . $e->getLine());
+}
 ?>
